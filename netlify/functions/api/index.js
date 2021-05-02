43963f5e2ed99b8db3a8ead518e68e73070885ad
Maciej Gibas <gibasmaciej@gmail.com>
2021-05-02 20:09:30 +0200

    Functions first go

---
+++ netlify/functions/api/index.js
@@ -0,0 +1,14 @@
+const methods = require('./methods')
+
+exports.handler = async (event, context) => {
+  const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '')
+  const segments = path.split('/').filter(e => e)
+  const method = event.httpMethod.toLowerCase()
+
+  console.log(event)
+  console.log(path)
+
+  return api[method]
+    ? api[method]({ path, event, context })
+    : { statusCode: 400, body: 'Unrecognized HTTP Method' }
+}
