7cb3b9772dc24dbd3f89734f1ad135bea2170097
Maciej Gibas <gibasmaciej@gmail.com>
2021-05-02 20:54:59 +0200

    Syntax

--- netlify/functions/api/index.js
+++ netlify/functions/api/index.js
@@ -3,7 +3,7 @@ const methods = require('./methods')
 exports.handler = async (event, context) => {
   const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '')
   const segments = path.split('/').filter(e => e)
-  const method = event.httpMethod?.toLowerCase() || 'get'
+  const method = event.httpMethod.toLowerCase() || 'get'
 
   console.log(event)
   console.log(path)
